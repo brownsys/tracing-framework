aeb33c321017b0e100ce7e6cc1b541141a76d329
Jonathan Mace <jonathan.c.mace@gmail.com>
2016-03-03 17:02:05 -0500

    Add hardcoded tracepoint to client library

---
+++ pivottracing/client/src/main/java/edu/brown/cs/systems/pivottracing/tracepoint/HardcodedTracepoint.java
@@ -0,0 +1,38 @@
+package edu.brown.cs.systems.pivottracing.tracepoint;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import edu.brown.cs.systems.pivottracing.advice.AdviceProtos.AdviceSpec;
+import edu.brown.cs.systems.pivottracing.agent.WeaveProtos.TracepointSpec;
+
+public class HardcodedTracepoint implements Tracepoint {
+    
+    private final String name, id;
+    private final List<String> exports;
+    
+    private HardcodedTracepoint(String name, String id, String... exports) {
+        this.name = name;
+        this.id = id;
+        this.exports = Lists.newArrayList(exports);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean exports(String varName) {
+        return exports.contains(varName);
+    }
+
+    @Override
+    public List<TracepointSpec> getTracepointSpecsForAdvice(AdviceSpec advice) {
+        TracepointSpec.Builder spec = TracepointSpec.newBuilder();
+        spec.getHardcodedTracepointBuilder().setId(id).addAllExport(exports);
+        return Lists.newArrayList(spec.build());
+    }
+
+}
