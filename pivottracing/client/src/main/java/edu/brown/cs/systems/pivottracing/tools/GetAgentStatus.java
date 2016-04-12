52b9274bfcbb027b4926f457981a38acd8b0e875
Jonathan Mace <jonathan.c.mace@gmail.com>
2016-04-12 11:52:31 -0400

    Update log4j properties file for pivottracing

--- pivottracing/client/src/main/java/edu/brown/cs/systems/pivottracing/tools/GetAgentStatus.java
+++ pivottracing/client/src/main/java/edu/brown/cs/systems/pivottracing/tools/GetAgentStatus.java
@@ -3,6 +3,9 @@ package edu.brown.cs.systems.pivottracing.tools;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.brown.cs.systems.pivottracing.PTAgentProtos.AgentStatus;
 import edu.brown.cs.systems.pivottracing.PTAgentProtos.PivotTracingCommand;
 import edu.brown.cs.systems.pivottracing.PivotTracingConfig;
@@ -12,12 +15,15 @@ import edu.brown.cs.systems.pubsub.PubSubClient.Subscriber;
 /** Just subscribes and prints all query results to stdout */
 public class GetAgentStatus extends Subscriber<AgentStatus> {
     
+    public static final Logger log = LoggerFactory.getLogger(GetAgentStatus.class);
+    
     @Override
     protected void OnMessage(AgentStatus message) {
         System.out.println(message);
     }
     
     public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
+        log.info("Getting agent status");
         PubSub.subscribe(PivotTracingConfig.STATUS_TOPIC, new GetAgentStatus());
         while (!Thread.currentThread().isInterrupted()) {
             PubSub.publish(PivotTracingConfig.COMMANDS_TOPIC, PivotTracingCommand.newBuilder().setSendStatus(true).build());
